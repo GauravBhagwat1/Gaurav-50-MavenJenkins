47c3da59216e25fb380779f0a7dc0786a72f0a87
Gaurav Bhagwat <gauravbhagwat91@gmail.com>
2026-03-23 20:59:52 +0530

    Project upload

---
+++ src/main/java/Jackins/Test.java
@@ -0,0 +1,18 @@
+package Jackins;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test 
+{
+	public static void main(String[] args) 
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.quit();
+	}
+
+}
